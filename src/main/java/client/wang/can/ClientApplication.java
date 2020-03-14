48dfb5e9cbbe45e041b215177cf8cd88b0210c4a
CAM <762092016@qq.com>
2020-03-14 10:28:43 +0800

    jar

--- src/main/java/client/wang/can/ClientApplication.java
+++ src/main/java/client/wang/can/ClientApplication.java
@@ -1,7 +1,9 @@
 package client.wang.can;
 
+import com.google.gson.Gson;
 import com.sun.org.apache.xpath.internal.operations.Or;
 import net.wang.can.profiles.Profile;
+import net.wang.can.protol.head.Head;
 import order.wang.can.CommandString;
 
 import java.io.File;
@@ -9,8 +11,11 @@ import java.net.Socket;
 
 public class ClientApplication {
     public static void main(String[] arge) throws Exception {
-        File file = new File("D:\\StudyWork\\test.txt");
-        OrderSolver.upload(file,Profile.FILE_SEPARATOR);
+        File file = new File("D:\\CAM.jks");
+        Head head = OrderSolver.upload(file,Profile.FILE_SEPARATOR+"testfilefolder"+Profile.FILE_SEPARATOR);
+        Gson gson = new Gson();
+        String s=  gson.toJson(head);
+        System.out.println(s);
 
 //        OrderSolver.downLoad("testNew\\1409.1556.pdf", new File("E:\\1409.1556.pdf"));
 
